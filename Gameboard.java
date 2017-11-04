f93783c649f8b1a86085b16e9cd8f4809f7b3fe9
clin <Nickles255@gmail.com>
2017-11-04 01:21:27 +0100

    Added my version of Gameboard.java

--- Gameboard.java
+++ Gameboard.java
@@ -1,12 +1,12 @@
 /**
   * Gameboard.java represents a gameboard to play tic tac toe as a two dimensional array.
-  * isDraw is working currently code could be improved. 
+  * 
   * 
   *
   *
   *  @author(s) Chien Lin, Lena Zheng, Qd Li
-  *  @version 1.1Q
-  *  @since 2017-11-03
+  *  @version 1.1c
+  *  @since 2017-11-3
 */
 
 import java.util.Scanner;
@@ -15,87 +15,92 @@ import java.util.Arrays;
 public class Gameboard { 
     final int bSize = 3;
     final String dashLine = "--------------";
-    private char[][] board = new char[bSize][bSize]; 
-
+    private char[][] board = new char[bSize][bSize];
     Scanner scan = new Scanner(System.in);
 
     public Gameboard(){
         for(int row = 0; row < board.length; row++)
         for (int col = 0; col < board[row].length; col++)
-            board[row][col] = ' ';
+             board[row][col] = ' ';
     }
 
     public void displayBoard(){
       
-        System.out.println(dashLine);
+        System.out.println(dashLine);     
         for(int row = 0; row < board.length; row++){
-            for (int col = 0; col < board[row].length; col++){
-            System.out.print( "| " + board[row][col] + " ");
+              for (int col = 0; col < board[row].length; col++){
+             System.out.print( "| " + board[row][col] + " ");
             }
 
             System.out.println("| \n" + dashLine);
-        }
-    };
+       }
+    }; 
 
     // Added setMarker to assist with setting character marking p_mark in p_row, p_col
     // of board
     public void setMarker(int p_row, int p_col, char p_mark){
         board[p_row][p_col] = p_mark;
     };
-    // to initiate player to make a move
-    public void makeAMove(char player){
-        System.out.println("Enter a row for player" +" "+ player+":");
-        int p_row=scan.nextInt();
-        System.out.println("enter a column for player" +" "+ player+":");
-        int p_col=scan.nextInt();
+
+    public void makeAMove(char myMark){
+    
+        int myRow, myCol;
+        System.out.println("Please enter row and column for your move: ");
         
-        while(board[p_row][p_col]!=' '){
-            System.out.println("row is occupied");
-            
-            System.out.println("Enter a row for player" +" "+ player+":");
-             p_row = scan.nextInt();
-               
-            System.out.println("enter a column for player" +" "+ player+":");
-             p_col = scan.nextInt();
-        }
-        setMarker(p_row,p_col,player); 
-    }
-    //determines a win if there is a three in a row
-    public boolean isWon(char player){
-        boolean match = false;
-        //check for win in a row    
-        for(int row=0; row<board.length;row++){
-            if(board[row][0]==player&&board[row][1]==player&&board[row][2]==player){
-                match=true;
-            }
-        //check for win in a column	
-            if(board[0][row]==player&&board[1][row]==player&&board[2][row]==player){
-                match=true;
-            }
-              
-        }
-        //check for win diagonal		     
-           if(board[0][0]==player&&board[1][1]==player&&board[2][2]==player){
-               match=true;
-           }
-           if(board[2][0]==player&&board[1][1]==player&&board[0][2]==player){
-               match=true;
-           }
+        System.out.print("Row: ");
+        myRow = scan.nextInt();
+        System.out.print("Column: ");
+        myCol = scan.nextInt();
 
-         return match;
-    }
-    //The method to determine if its a draw or not   
-    public  boolean isDraw(){
-        boolean draw = false;
-        for(int row = 0; row<board.length;row++){
-            for(int col = 0;col<board[row].length;col++){
-		//if the row and column not empty and there is no winner, there is a draw
-                if(board[row][col]!=' '&&isWon('x')==draw&&isWon('o')){
-                   draw= true;               
-                }
-            }
+        while(board[myRow][myCol] != ' '){ 
+            System.out.println("Row " + myRow + " Column " + myCol + " is already taken!");
+            System.out.println("Please enter another location.");
+            System.out.print("Row: ");
+            myRow = scan.nextInt();
+            System.out.print("Column: ");
+            myCol = scan.nextInt();
         }
-        return draw;
-    }
 
+        setMarker(myRow, myCol, myMark); 
+    };
+
+    public boolean isWon(char myMark){
+            boolean return_value = false;
+ 
+        // Testing vertical and horizontal win situations.
+        for(int rc = 0; rc < bSize; rc++){
+           
+          if(board[rc][0] != ' ' && (board[rc][0] == board[rc][1]) && (board[rc][1] == board[rc][2]))
+              return_value = true;  
+        
+          if(board[0][rc] != ' ' && (board[0][rc] == board[1][rc]) && (board[1][rc] == board[2][rc]))
+              return_value = true;
+        }
+
+        // Testing two diagonal win situations.
+        if(board[0][0] != ' ' && (board[0][0] == board[1][1]) && (board[1][1] == board[2][2]))
+           return_value = true;
+
+        if(board[0][2] != ' ' && (board[0][2] == board[1][1]) && (board[1][1] == board[2][0]))
+           return_value = true;
+
+          return return_value;
+    };
+
+    public boolean isDraw(){
+      int num_spaces = 0;
+      boolean return_value = false;
+      // count number of spaces in the array is blank.
+      for(int row = 0; row < bSize; row++){
+         for (int col = 0; col < bSize; col++){
+            if(board[row][col] == ' ')
+                num_spaces++;
+         }
+      }
+      
+      if(num_spaces == 0 && isWon('X') == false && isWon('O') == false)
+         return_value = true;
+
+     return return_value;
+    };
 }
